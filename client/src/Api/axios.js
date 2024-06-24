6bd7ba20a815d87ac5272093e34d4cf0109721ac
BrianLigh <brianlight2002@yahoo.com>
2024-06-24 01:21:58 -0700

    code: Axios BaseURL set to deployed url version

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5500/api/",
+  baseURL: "https://evangadi-forum-starter.onrender.com/api/",
 });
 
 export { axiosInstance };
